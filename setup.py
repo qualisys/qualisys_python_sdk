22ca236da17e55239586261d055579abd0c66ef6
Martin Gejke <martin.gejke@qualisys.se>
2018-09-19 10:49:57 +0200

    Added data file to package

--- setup.py
+++ setup.py
@@ -1,25 +1,29 @@
 from setuptools import setup
 
-version = '2.0.0'
+version = "2.0.1"
 
 setup(
-    name='qtm',
+    name="qtm",
     version=version,
-    description='QTM Python SDK',
-    url='https://github.com/qualisys/qualisys_python_sdk',
-    download_url='https://github.com/qualisys/qualisys_python_sdk/tarball/{}'.
-    format(version),
-    author='Martin Gejke',
-    author_email='support@qualisys.com',
-    license='MIT',
-    packages=['qtm'],
+    description="QTM Python SDK",
+    url="https://github.com/qualisys/qualisys_python_sdk",
+    download_url="https://github.com/qualisys/qualisys_python_sdk/tarball/{}".format(
+        version
+    ),
+    author="Martin Gejke",
+    author_email="support@qualisys.com",
+    license="MIT",
+    packages=["qtm"],
+    package_data={"qtm": ["data/demo.qtm"]},
     classifiers=[
-        'Operating System :: OS Independent',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Topic :: Scientific/Engineering', 'Topic :: Utilities'
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Utilities",
     ],
-    python_requires='>=3.5.3',
-    zip_safe=True)
+    python_requires=">=3.5.3",
+    zip_safe=True,
+)
