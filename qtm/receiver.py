a3b12e707759720394a8c9301640163c1a563a47
Johannes Gotlén <johannes.gotlen@qualisys.se>
2021-07-12 11:18:55 +0200

    Added size check before unpacking RTheader

--- qtm/receiver.py
+++ qtm/receiver.py
@@ -16,18 +16,17 @@ class Receiver(object):
         """ Received from QTM and route accordingly """
         self._received_data += data
         h_size = RTheader.size
-
         data = self._received_data
-        size, type_ = RTheader.unpack_from(data, 0)
+        data_len = len(data);
 
-        while len(data) >= size:
-            self._parse_received(data[h_size:size], type_)
-            data = data[size:]
-
-            if len(data) < h_size:
-                break
-
+        while data_len >= h_size:
             size, type_ = RTheader.unpack_from(data, 0)
+            if data_len >= size:
+                self._parse_received(data[h_size:size], type_)
+                data = data[size:]
+                data_len = len(data);
+            else:
+                break;
 
         self._received_data = data
 
