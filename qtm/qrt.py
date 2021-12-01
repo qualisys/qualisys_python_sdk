73443f72eb6822000e25d30e026153c3954bd3ac
Filip Pentikäinen <filip.pentikainen@qualisys.se>
2021-12-01 17:33:29 +0100

    increment version number

--- qtm/qrt.py
+++ qtm/qrt.py
@@ -327,7 +327,7 @@ class QRTConnection(object):
 async def connect(
     host,
     port=22223,
-    version="1.22",
+    version="1.23",
     on_event=None,
     on_disconnect=None,
     timeout=5,
