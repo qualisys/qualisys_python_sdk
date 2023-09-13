4e0484e76ab0ec6cca767509dd29a0680b4af9c1
Martin Holmberg <martin.holmberg@qualisys.se>
2023-09-13 17:53:20 +0200

    Updated comments and formatting

--- examples/control_example.py
+++ examples/control_example.py
@@ -1,4 +1,7 @@
-""" Example that takes control of QTM, starts a measurment and stops after 10 seconds and saves the measurement to a file """
+"""
+Example that takes control of QTM, starts a measurement and stops after
+10 seconds, then saves the measurement data to a file.
+"""
 
 import asyncio
 import logging
@@ -10,7 +13,10 @@ LOG = logging.getLogger("example")
 
 
 async def setup():
-    """ Main function that connects to the QTM server, starts a measurement, and saves the data to a file """
+    """ 
+    Main function that connects to the QTM server, initiates a measurement
+    process, and saves the captured data to a file.
+    """
     connection = await qtm_rt.connect("127.0.0.1")
 
     if connection is None:
@@ -25,15 +31,19 @@ async def setup():
             except asyncio.TimeoutError:
                 LOG.error("Failed to start new measurement")
                 return -1
+            
         # Start the measurement
         await connection.start()
         await connection.await_event(qtm_rt.QRTEvent.EventCaptureStarted, timeout=10)
 
-        # Wait 10 seconds
+        # Wait for QTM to measure for 10 seconds
         await asyncio.sleep(10)
 
+        # Stop the measurement
         await connection.stop()
         await connection.await_event(qtm_rt.QRTEvent.EventCaptureStopped, timeout=10)
+        
+        # Save the measurement to a file
         await connection.save("Demo.qtm")
 
         LOG.info("Measurement saved to Demo.qtm")
